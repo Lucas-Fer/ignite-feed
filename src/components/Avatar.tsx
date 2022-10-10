8ff59349469f9e42ec1708ca338ba45954ac0dd2
lucas <fernandes20lucas@gmail.com>
2022-10-10 18:36:38 -0300

    fix: error

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -5,14 +5,12 @@ interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   commentComponent?: boolean;
 }
 
-export default class Avatar extends Component {
-  render() {
-    const avatarProps: AvatarProps = this.props;
+export default function Avatar({avatarProps}: any) {
     return (
       <img
         className={avatarProps.commentComponent ? styles.avatarWithoutBorder : styles.avatar}
         src={avatarProps.src}
       />
     )
-  }
+
 }
